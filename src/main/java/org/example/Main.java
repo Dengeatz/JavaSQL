6a954edb7b372b40798c6f158a9299e0cded9b7c
Dengeatz <gameone.exe@gmail.com>
2024-03-10 12:27:57 +0500

    New getters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,9 @@ public class Main {
     static User user = new User(5);
 
     public static void main(String[] args) {
-        System.out.println(user.getName());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Email: " + user.getEmail());
+        System.out.println("Password: " + user.getPassword());
+        System.out.println("Id: " + user.getId());
     }
 }
